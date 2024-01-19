4c9cad01e06707e043ef6dc2c10dcdbff6f5a22d
Denis Tsariov <tsariov.den@gmail.com>
2024-01-19 16:37:20 -0500

    chap done

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Denis Tsariov on 2023-08-23.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
